f238e2060f0a46d3b104391736a6fd27f08cae64
PrinceLee1 <majornwa189@gmail>
2025-09-20 14:36:02 +0100

    added seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'onyebuchiprincee@gmail.com',
+            'password' => Hash::make('12345678'), // always hash!
+        ]);
+
+        // You can add more users here
+        User::factory()->count(2)->create(); // generates 5 random users
+    }
+}
